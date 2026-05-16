6c8d19973e6bd9f55ce53e09280490054b92ea99
BholendraPratap <bholendrapratap.me@gmail.com>
2026-05-17 00:00:29 +0530

    db issue fix

--- remote_server.py
+++ remote_server.py
@@ -7,9 +7,7 @@ import asyncio
 # Use temporary directory which should be writable
 TEMP_DIR = tempfile.gettempdir()
 # DB_PATH = os.path.join(TEMP_DIR, "expenses.db")
-#DB_PATH = os.path.join("/tmp", "expenses.db")
-DB_PATH = "/data/expenses.db"
-os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+DB_PATH = os.path.join("/tmp", "expenses.db")
 CATEGORIES_PATH = os.path.join(os.path.dirname(__file__), "categories.json")
 
 print(f"Database path: {DB_PATH}")
